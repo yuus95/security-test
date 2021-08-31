5510bb3d00edfb00e2b988aca715921272c1f5c9
yuus95 <kkad45@naver.com>
2021-08-31 20:50:04 +0900

    Oauth2 jwt 적용실패 다시 알아보기

--- src/main/java/com/example/security/repository/UserRepository.java
+++ src/main/java/com/example/security/repository/UserRepository.java
@@ -4,8 +4,10 @@ package com.example.security.repository;
 import com.example.security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
-
+    Optional<User> findByEmail(String email);
 
 }
